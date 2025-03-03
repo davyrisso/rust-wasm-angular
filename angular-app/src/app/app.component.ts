5b4639971818d1040731db5d8b97c4ef0a511f57
Davy Risso <davyrisso@google.com>
2025-03-04 00:51:42 +0100

    fix: adjust layout styles to ensure proper viewport size

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ViewportComponent } from './viewport/viewport.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent, ViewportComponent],
+  imports: [
+    CommonModule,
+    SidebarComponent,
+    ViewportComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
